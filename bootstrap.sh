b475bcf241f13c9d9f91d0ab778d4970b7574953
Sebastian Vaisov <sebastjanas@vaisovas.lt>
2020-08-20 14:39:20 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get dist-upgrade
-sudo apt-get install parallel dns-utils 
+sudo apt-get -y dist-upgrade
+sudo apt-get install -y parallel dns-utils libevent ncurses libevent-dev ncurses-dev build-essential bison pkg-config wget curl vim mc
+
+wget https://github.com/tmux/tmux/releases/download/3.1b/tmux-3.1b.tar.gz
+tar -zxf tmux-*.tar.gz
+cd tmux-*/
+./configure
+make && sudo make install
+cd -
